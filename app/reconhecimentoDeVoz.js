0b02eeda561e61db6d85d0e4f98b68a5c110753f
Andressa Da Costa <andressadacostan@gmail.com>
2022-12-15 14:35:29 -0300

    guess

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -3,14 +3,13 @@ const elementoChute = document.getElementById('chute');
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-
-recognition.lang = 'pt-br';
+recognition.lang = 'pt-Br';
 recognition.start();
 
 recognition.addEventListener('result', onSpeak);
 
 function onSpeak(e) {
-	console.log(e.results[0][0].transcript);
+	chute = e.results[0][0].transcript;
 	exibeChuteNaTela(chute);
 	verificaSeOChutePossuiUmValorValido(chute);
 }
